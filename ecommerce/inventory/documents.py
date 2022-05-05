4344eb045fd3fe46eebf1424302142923b9ea646
Lin Domacia <layard3@gmail.com>
2022-05-05 15:00:26 +0800

    updated elasticsearch documents

--- ecommerce/inventory/documents.py
+++ ecommerce/inventory/documents.py
@@ -1,11 +1,22 @@
-from pydoc import Doc
-from django_elasticsearch_dsl import Document
+from django_elasticsearch_dsl import Document, fields
 from django_elasticsearch_dsl.registries import registry
 
 from .models import ProductInventory
 
 @registry.register_document
 class ProductInventoryDocument(Document):
+
+    product = fields.ObjectField(
+        properties={
+            "name": fields.TextField()
+        }
+    )
+
+    product_inventory = fields.ObjectField(
+        properties={
+            "units": fields.IntegerField(),
+        }
+    )
 
     class Index:
         name = "productinventory"
