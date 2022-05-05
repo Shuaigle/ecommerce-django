874bfaab9d680ccf8563dad93f14d10323010110
Lin Domacia <layard3@gmail.com>
2022-05-05 15:10:37 +0800

    built restful api

---
+++ ecommerce/drf/serializer.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from ecommerce.inventory.models import Product
+
+class AllProducts(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = "__all__"
