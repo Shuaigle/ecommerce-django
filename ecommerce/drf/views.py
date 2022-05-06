36a0fde0a2972a1a43371b90ea02f90d6ff522e0
Lin Domacia <layard3@gmail.com>
2022-05-06 11:01:27 +0800

    added restful api

--- ecommerce/drf/views.py
+++ ecommerce/drf/views.py
@@ -1,2 +1,9 @@
-from django.shortcuts import render
+from ecommerce.inventory.models import Product
+from rest_framework import mixins, permissions, viewsets
+from ecommerce.drf.serializer import AllProducts
 
+class AllProductsViewsets(viewsets.ModelViewSet):
+
+    queryset = Product.objects.all()[:10]
+    serializer_class = AllProducts
+    permissions_classes = [permissions.IsAuthenticatedOrReadOnly]
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+
+from ecommerce.drf.views import AllProductsViewsets
+
+router = routers.DefaultRouter()
+router.register(
+    r'home', AllProductsViewsets, basename="allproducts"
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('demo/', include(("ecommerce.demo.urls", "demo"), namespace="demo"))
+    path('demo/', include(("ecommerce.demo.urls", "demo"), namespace="demo")),
+    path("", include(router.urls)),
 ]
